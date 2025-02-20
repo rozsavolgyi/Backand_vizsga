6218037f3c2f21130b98af8f8cd55568afc619d6
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-20 18:06:46 +0100

    getTavak

--- controllers/tavak.js
+++ controllers/tavak.js
@@ -1,3 +1,12 @@
 const tavakModel = require("../models/TavakModel");
 const FogasokModel=require("../models/FogasokModel")
 const ErrorResponse = require("../utils/errorResponse");
+
+exports.getTavak = async (req, res, next) => {
+    try {
+      const tavak = await tavakModel.find(req.query)
+      res.status(200).json({ success: true, count: tavak.length, data: tavak })
+  } catch (error) {
+      res.status(500).json({ success: false })
+  }
+  };
